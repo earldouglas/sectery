52a53aeba66e728023cd7eb430dcc0869d7c17c4
James Earl Douglas <james@earldouglas.com>
2015-01-10 18:45:36 -0800

    Fix broken coverage config

--- sectery.js
+++ sectery.js
@@ -2,7 +2,10 @@
 
 var irc = require('irc');
 var fsdb = require('./lib/fsdb');
-var main = require('./lib/main');
+
+// Use instrumented code for code coverage tests                                 
+var lib = process.env.LIB_COV ? 'lib-cov' : 'lib';                               
+var main     = require('./' + lib + '/main');                                
 
 var config = fsdb.load('config.json');
 var client = new irc.Client(config.irc.host, config.irc.user, config.irc.opts);
