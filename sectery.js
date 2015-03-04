ac94dc4592aa1b510f0859b7f989489d1450f90c
James Earl Douglas <james@earldouglas.com>
2015-03-03 19:47:30 -0800

    Add channels support

--- sectery.js
+++ sectery.js
@@ -5,7 +5,7 @@ var sectery = require('./lib/sectery');
 
 var client;
 
-[ 'IRC_HOST', 'IRC_USER', 'IRC_PASS', ].forEach(function (x) {
+[ 'IRC_HOST', 'IRC_USER', 'IRC_PASS', 'IRC_CHANNELS', ].forEach(function (x) {
   if (process.env[x] === undefined) {
       console.log('Please set ' + x + ' and try again.');                      
       process.exit();                                                                
@@ -15,7 +15,10 @@ var client;
 var client = new irc.Client(
   process.env.IRC_HOST,
   process.env.IRC_USER,
-  { password: process.env.IRC_PASS }
+  {
+    password: process.env.IRC_PASS,
+    channels: process.env.IRC_CHANNELS.split(',')
+  }
 );
 
 sectery(client);
