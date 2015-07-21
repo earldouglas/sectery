60df7c2e47c3065682ebac84771bc578a2777c70
Freddie Sanchez <freddie.sanchez@gmail.com>
2015-07-21 07:01:10 -0700

    updated to correct error message

--- test/sectry_test.js
+++ test/sectry_test.js
@@ -471,15 +471,15 @@ exports.sectery = {
     var user1 = Math.random().toString(36).substr(2, 5);
     client._message(user1, '#test-channel', '@cron');
     test.equal(client._lastSaid().to, '#test-channel');
-    test.equal(client._lastSaid().message, 'Usage: @cron <add|remove> <cron-string> <message>|<id>' );
+    test.equal(client._lastSaid().message, 'Usage: @cron <add|remove> "<cron-string>" <message>|<id>' );
 
     client._message(user1, '#test-channel', '@cron add "bogus input" test');
     test.equal(client._lastSaid().to, '#test-channel');
-    test.equal(client._lastSaid().message, 'Error: '+ user1 + ': The cron string "bogus input" is not valid. Usage: @cron <add|remove> <cron-string> <message>|<id>' );
+    test.equal(client._lastSaid().message, 'Error: '+ user1 + ': The cron string "bogus input" is not valid. Usage: @cron <add|remove> "<cron-string>" <message>|<id>' );
 
     client._message(user1, '#test-channel', '@cron remove 1');
     test.equal(client._lastSaid().to, '#test-channel');
-    test.equal(client._lastSaid().message, 'Error: '+ user1 + ': The cron job with id "1" was not found. Usage: @cron <add|remove> <cron-string> <message>|<id>' );
+    test.equal(client._lastSaid().message, 'Error: '+ user1 + ': The cron job with id "1" was not found. Usage: @cron <add|remove> "<cron-string>" <message>|<id>' );
     
     client._message(user1, '#test-channel', '@cron add "* * * * * *" This is cool.');
     test.equal(client._lastSaid().to, '#test-channel');
@@ -509,7 +509,7 @@ exports.sectery = {
 
   },
   '@cron [cmd]': function(test) {
-    test.expect(4);
+    test.expect(6);
     
     var user1 = Math.random().toString(36).substr(2, 5);
     client._message(user1, '#test-channel', '@cron add "* * * * * *" @simpsons');
@@ -520,6 +520,9 @@ exports.sectery = {
       test.equal(client._lastSaid().to, '#test-channel');
       test.equal(client._lastSaid().message, "(S2E1): We have time for one more report. Bart Simpson? ");
 
+      client._message(user1, '#test-channel', '@cron remove 2');
+      test.equal(client._lastSaid().to, '#test-channel');
+      test.equal(client._lastSaid().message, user1 + ': OK - cron job 2 stopped!' );
       test.done();
     }, 2000);
 
