13cb9b18c88daa4e864c776423355f2491a93b29
James Earl Douglas <james@earldouglas.com>
2015-07-28 14:20:57 -0700

    Get test user nicks from irc

--- test/sectry_test.js
+++ test/sectry_test.js
@@ -7,13 +7,11 @@ var irc       = require('irc');
 
 var assert   = require('assert');
 
-function user(suffix) {
-
-  var nick = 'sectery|' + suffix;
+function user() {
 
   var client = new irc.Client(
     'irc.freenode.net',
-    nick,
+    'sectery|test',
     {
       password: process.env.IRC_PASS,
       channels: ['#sectery'],
@@ -21,7 +19,7 @@ function user(suffix) {
   );
 
   client.addListener('error', function (message) {
-    console.log(suffix, 'error', messsage);
+    console.log(client.nick, 'error', messsage);
   });
 
   var expectM = function (done, expectedFrom, messagePredicate) {
@@ -36,7 +34,7 @@ function user(suffix) {
   };
 
   return {
-    nick: nick,
+    nick: function () { return client.nick; },
     expectM: expectM,
     expectMessage: function (done, expectedFrom, expectedMessage) {
       return expectM(done, expectedFrom, function (x) {
@@ -63,7 +61,7 @@ function user(suffix) {
       setTimeout(function () { client.say('#sectery', x); }, 2000);
     },
     privateMessage: function (x) {
-      setTimeout(function () { client.say(secteryUser.nick, x); }, 2000);
+      setTimeout(function () { client.say(secteryUser.nick(), x); }, 2000);
     },
     part: function (k) {
       client.part('#sectery', k);
@@ -75,11 +73,10 @@ function user(suffix) {
 
 }
 
-var secteryUser = user('test');
-var testUser = user('test1');
-var testUser2 = user('test2');
+var secteryUser = user();
+var testUser = user();
+var testUser2 = user();
 
-process.env.IRC_USER = secteryUser.nick;
 var replyInterval = sectery(secteryUser.client);
 
 describe('sectery', function () {
@@ -87,7 +84,8 @@ describe('sectery', function () {
 
   it('should wait for sectery to join', function (done) {
     var joinListener = function (channel, nick, message) {
-      if (nick === secteryUser.nick) {
+      if (nick === secteryUser.nick()) {
+        process.env.IRC_USER = nick;
         secteryUser.client.removeListener('join', joinListener);
         done();
       }
@@ -97,57 +95,57 @@ describe('sectery', function () {
 
   it('should greet user upon joining', function (done) {
     testUser.part(function (nick) {
-      testUser.expectMessage(done, secteryUser.nick, 'Hey, ' + testUser.nick + '!');
+      testUser.expectMessage(done, secteryUser.nick(), 'Hey, ' + testUser.nick() + '!');
       testUser.join('#sectery');
     });
   });
 
   it('@all', function (done) {
-    testUser.expectM(done, secteryUser.nick, function (x) {
-      var substr = testUser.nick;
+    testUser.expectM(done, secteryUser.nick(), function (x) {
+      var substr = testUser.nick();
       return x.indexOf(substr >= 0);
     });
     testUser.message('@all');
   });
 
   it('emoji', function(done) {
-    testUser.expectMessage(done, secteryUser.nick, '╯°□°）╯︵ ┻━┻');
+    testUser.expectMessage(done, secteryUser.nick(), '╯°□°）╯︵ ┻━┻');
     testUser.message('foo bar table flip baz');
   });
 
   it('html title with numeric http code(s)', function(done) {
-    testUser.expectMessage(done, secteryUser.nick,
+    testUser.expectMessage(done, secteryUser.nick(),
       'é HTML Entity code in title tags - Stack Overflow');
     testUser.message('http://stackoverflow.com/questions/11037123/' +
       '%C3%A9-html-entity-code-in-title-tags');
   });
 
   it('html title', function(done) {
-    testUser.expectMessage(done, secteryUser.nick, 'James Earl Douglas');
+    testUser.expectMessage(done, secteryUser.nick(), 'James Earl Douglas');
     testUser.message('http://earldouglas.com/');
   });
 
   it('[pm] @echo', function(done) {
-    testUser.expectPM(done, secteryUser.nick, 'ping');
+    testUser.expectPM(done, secteryUser.nick(), 'ping');
     testUser.privateMessage('@echo ping');
   });
 
   it('[pm] @setup - usage', function(done) {
     var message = "Usage: @setup <email|sms> <email@example.com|phone|code>";
-    testUser.expectPM(done, secteryUser.nick, message);
+    testUser.expectPM(done, secteryUser.nick(), message);
     testUser.privateMessage('@setup');
   });
 
   it('@tell (set)', function(done) {
-    testUser.expectMessage(done, secteryUser.nick, "I'll pass your message along.");
-    testUser.message('@tell ' + testUser2.nick + ' Welcome back!');
+    testUser.expectMessage(done, secteryUser.nick(), "I'll pass your message along.");
+    testUser.message('@tell ' + testUser2.nick() + ' Welcome back!');
   });
 
   it('@tell (get)', function(done) {
     testUser2.part(function (nick) {
       testUser2.join(function (nick) {
-        testUser2.expectMessageR(done, secteryUser.nick,
-          new RegExp(testUser2.nick + ': ' + testUser.nick +
+        testUser2.expectMessageR(done, secteryUser.nick(),
+          new RegExp(testUser2.nick() + ': ' + testUser.nick() +
             ' said "Welcome back!"'));
         testUser2.message('Hey, everyone!');
       });
@@ -155,93 +153,93 @@ describe('sectery', function () {
   });
 
   it('@note (usage)', function(done) {
-    testUser.expectMessage(done, secteryUser.nick, 'Usage: @note <message>');
+    testUser.expectMessage(done, secteryUser.nick(), 'Usage: @note <message>');
     testUser.message('@note');
   });
 
   it('@note (no email)', function(done) {
-    testUser.expectMessage(done, secteryUser.nick, testUser.nick +
+    testUser.expectMessage(done, secteryUser.nick(), testUser.nick() +
       ': PM me your email address with: /msg ' +
-      secteryUser.nick + ' @setup email name@example.com');
+      secteryUser.nick() + ' @setup email name@example.com');
     testUser.message('@note Testing is hard.');
   });
 
   it.skip('@ascii art', function(done) {
-    testUser.expectMessage(done, secteryUser.nick, '[ascii art]');
+    testUser.expectMessage(done, secteryUser.nick(), '[ascii art]');
     testUser.message('@ascii http://example.com/test.png');
   });
 
   it('@simpsons', function(done) {
-    testUser.expectMessageR(done, secteryUser.nick, /^\(S\d+E\d+\): /);
+    testUser.expectMessageR(done, secteryUser.nick(), /^\(S\d+E\d+\): /);
     testUser.message('@simpsons');
   });
 
   it('@cards', function (done) {
-    testUser.expectMessageR(done, secteryUser.nick,
+    testUser.expectMessageR(done, secteryUser.nick(),
       /^(\d+,?\s+){5}Objective\s+Card:\s+\d+$/);
     testUser.message('@cards');
   });
 
   it('@krypto (premature @guess)', function(done) {
-    testUser.expectMessage(done, secteryUser.nick,
-      testUser.nick + ': please say "@krypto" first!');
+    testUser.expectMessage(done, secteryUser.nick(),
+      testUser.nick() + ': please say "@krypto" first!');
     testUser.message('@guess');
   });
 
   it('@krypto', function(done) {
-    testUser.expectMessage(done, secteryUser.nick,
-      testUser.nick + ': OK - take a guess.');
+    testUser.expectMessage(done, secteryUser.nick(),
+      testUser.nick() + ': OK - take a guess.');
     testUser.message('@krypto');
   });
 
   it('@krypto (wrong user)', function(done) {
-    testUser2.expectMessage(done, secteryUser.nick,
-      testUser2.nick + ": sorry, but it's " + testUser.nick + "'s turn.");
+    testUser2.expectMessage(done, secteryUser.nick(),
+      testUser2.nick() + ": sorry, but it's " + testUser.nick() + "'s turn.");
     testUser2.message('@guess');
   });
 
   it('@cron (add)', function(done) {
-    testUser2.expectMessageR(done, secteryUser.nick,
-      new RegExp(testUser2.nick + ': OK - cron job \d+ scheduled!'));
+    testUser2.expectMessageR(done, secteryUser.nick(),
+      new RegExp(testUser2.nick() + ': OK - cron job \d+ scheduled!'));
     testUser2.message('@cron add "* * * * * *" This is cool.');
   });
 
   it('@cron (ls)', function(done) {
-    testUser2.expectMessageR(done, secteryUser.nick,
+    testUser2.expectMessageR(done, secteryUser.nick(),
       /\d+: "\* \* \* \* \* \*" "This is cool."/);
     testUser2.message('@cron ls');
   });
 
   it('@cron (remove)', function(done) {
-    testUser2.expectMessageR(done, secteryUser.nick,
-      new RegExp(testUser2.nick + ': OK - cron job \d+ stopped!'));
+    testUser2.expectMessageR(done, secteryUser.nick(),
+      new RegExp(testUser2.nick() + ': OK - cron job \d+ stopped!'));
     testUser2.message('@cron remove 0');
   });
 
   it('@time', function(done) {
-    testUser2.expectMessageR(done, secteryUser.nick,
+    testUser2.expectMessageR(done, secteryUser.nick(),
       /until end of next workday./);
     testUser2.message('@time');
   });
 
   it('regex', function(done) {
-    testUser2.expectMessage(done, secteryUser.nick,
-      '<' + testUser2.nick + '>: bar');
+    testUser2.expectMessage(done, secteryUser.nick(),
+      '<' + testUser2.nick() + '>: bar');
     testUser2.message('foo');
     testUser2.message('s/foo/bar/');
   });
 
   it('@grab', function(done) {
-    testUser2.expectMessage(done, secteryUser.nick,
-      testUser2.nick + ': OK - message grabbed.');
+    testUser2.expectMessage(done, secteryUser.nick(),
+      testUser2.nick() + ': OK - message grabbed.');
     testUser.message('bananas');
-    testUser2.message('@grab ' + testUser.nick);
+    testUser2.message('@grab ' + testUser.nick());
   });
 
   it('@quote', function(done) {
-    testUser2.expectMessageR(done, secteryUser.nick,
-      new RegExp('<' + testUser.nick + '>: bananas'));
-    testUser2.message('@quote ' + testUser.nick);
+    testUser2.expectMessageR(done, secteryUser.nick(),
+      new RegExp('<' + testUser.nick() + '>: bananas'));
+    testUser2.message('@quote ' + testUser.nick());
   });
 
 });
