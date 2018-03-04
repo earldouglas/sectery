b68b655e0cb011041f033631fe2e1dba20d6bcb4
James Earl Douglas <james@earldouglas.com>
2018-03-04 16:29:21 -0700

    Add @wx using Dark Sky API

--- sectery.js
+++ sectery.js
@@ -1,7 +1,7 @@
 var sectery = require('./lib/sectery');
 var client = require('./lib/client');
 
-[ 'IRC_ENV', 'IRC_HOST', 'IRC_USER', 'IRC_PASS', 'IRC_CHANNELS', ].forEach(
+[ 'DARK_SKY_API_KEY', 'IRC_ENV', 'IRC_HOST', 'IRC_USER', 'IRC_PASS', 'IRC_CHANNELS', ].forEach(
   function (x) {
     if (process.env[x] === undefined) {
         console.log('Please set ' + x + ' and try again.');
