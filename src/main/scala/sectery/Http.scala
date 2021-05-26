8a65ee2573e35d6e9ab9bfd0686cda12ab702743
James Earl Douglas <james@earldouglas.com>
2021-05-26 05:21:00 -0700

    Add support for @eval and an HTTP ZIO service
    This adds support for `@eval`, which passes its argument to Haskeval.
    This also adds a new ZIO service for making HTTP requests, and a way to
    mock responses for ZIO Test.

---
+++ src/main/scala/sectery/Http.scala
@@ -0,0 +1,96 @@
+package sectery
+
+import java.net.HttpURLConnection
+import java.net.URL
+import scala.collection.JavaConverters._
+import scala.io.Source
+import zio.Has
+import zio.UIO
+import zio.ULayer
+import zio.URIO
+import zio.ZIO
+import zio.ZLayer
+
+case class Response(
+  status: Int,
+  headers: Map[String, String],
+  body: String
+)
+
+object Http:
+
+  type Http = Has[Http.Service]
+
+  trait Service:
+    def request(
+      method: String,
+      url: String,
+      headers: Map[String, String],
+      body: Option[String]
+    ): UIO[Response]
+
+  val live: ULayer[Has[Service]] =
+    ZLayer.succeed {
+      new Service:
+        def request(
+          method: String,
+          url: String,
+          headers: Map[String, String],
+          body: Option[String]
+        ): UIO[Response] =
+
+          ZIO.effectTotal {
+            val c =
+              new URL(url)
+                .openConnection()
+                .asInstanceOf[HttpURLConnection]
+
+            c.setInstanceFollowRedirects(false)
+            c.setRequestMethod(method)
+            c.setDoInput(true)
+            c.setDoOutput(body.isDefined)
+
+            headers foreach {
+              case (k, v) =>
+                c.setRequestProperty(k, v)
+            }
+
+            body foreach { b =>
+              c.getOutputStream.write(b.getBytes("UTF-8"))
+            }
+
+            val response =
+              Response(
+                status = c.getResponseCode(),
+                headers =
+                  c
+                    .getHeaderFields()
+                    .asScala
+                    .filter({ case (k, _) => k != null })
+                    .map({ case (k, v) => (k, v.asScala.mkString(",")) })
+                    .toMap - "Date" - "Content-Length" - "Server",
+                body =
+                  Source
+                    .fromInputStream {
+                      if (c.getResponseCode() < 400) {
+                        c.getInputStream
+                      } else {
+                        c.getErrorStream
+                      }
+                    }
+                    .mkString
+              )
+
+            c.disconnect()
+
+            response
+          }
+    }
+
+  def request(
+    method: String,
+    url: String,
+    headers: Map[String, String],
+    body: Option[String]
+  ): URIO[Http, Response] =
+    ZIO.accessM(_.get.request(method, url, headers, body))
--- src/test/scala/sectery/MessageQueuesSpec.scala
+++ src/test/scala/sectery/MessageQueuesSpec.scala
@@ -7,39 +7,80 @@ import zio.duration._
 import zio.test._
 import zio.test.Assertion.equalTo
 import zio.test.environment.TestClock
+import zio.test.TestAspect._
+import zio.ULayer
+import zio.ZLayer
 
 /**
  * A [[Sender]] implementation for testing.  Saves "sent" messages to a
  * queue for later inspection.
  */
-class MessageLogger(queue: Queue[Tx]) extends Sender {
+class MessageLogger(queue: Queue[Tx]) extends Sender:
   def send(m: Tx): UIO[Unit] =
     queue.offer(m) *> ZIO.unit
-}
 
 /**
  * Tests the management of the receive and send message queues.
  */
-object MessageQueuesSpec extends DefaultRunnableSpec {
+object MessageQueuesSpec extends DefaultRunnableSpec:
   def spec = suite("MessageQueuesSpec")(
     testM("@ping produces pong") {
       for
         sent   <- ZQueue.unbounded[Tx]
-        inbox  <- MessageQueues.loop(new MessageLogger(sent))
+        inbox  <- MessageQueues.loop(new MessageLogger(sent)).provideLayer(Clock.any ++ TestHttp())
         _      <- inbox.offer(Rx("#foo", "bar", "@ping"))
         _      <- TestClock.adjust(1.seconds)
         m      <- sent.take
       yield assert(m)(equalTo(Tx("#foo", "pong")))
-    },
+    } @@ timeout(2.seconds),
     testM("@time produces time") {
       for
         sent   <- ZQueue.unbounded[Tx]
-        inbox  <- MessageQueues.loop(new MessageLogger(sent))
+        inbox  <- MessageQueues.loop(new MessageLogger(sent)).provideLayer(Clock.any ++ TestHttp())
         _      <- TestClock.setTime(1234567890.millis)
         _      <- inbox.offer(Rx("#foo", "bar", "@time"))
         _      <- TestClock.adjust(1.seconds)
         m      <- sent.take
       yield assert(m)(equalTo(Tx("#foo", "Wed, 14 Jan 1970, 23:56 MST")))
-    }
+    } @@ timeout(2.seconds),
+    testM("@eval produces result") {
+      for
+        sent   <- ZQueue.unbounded[Tx]
+        inbox  <- MessageQueues.loop(new MessageLogger(sent)).provideLayer(Clock.any ++ TestHttp(200, Map.empty, "42"))
+        _      <- inbox.offer(Rx("#foo", "bar", "@eval 6 * 7"))
+        _      <- TestClock.adjust(1.seconds)
+        m      <- sent.take
+      yield assert(m)(equalTo(Tx("#foo", "42")))
+    } @@ timeout(2.seconds)
   )
-}
+
+object TestHttp:
+
+  def apply(): ULayer[Has[Http.Service]] =
+    apply(
+      resStatus = 200,
+      resHeaders = Map.empty,
+      resBody = ""
+    )
+
+  def apply(
+    resStatus: Int,
+    resHeaders: Map[String, String],
+    resBody: String
+  ): ULayer[Has[Http.Service]] =
+    ZLayer.succeed {
+      new Http.Service:
+        def request(
+          method: String,
+          url: String,
+          headers: Map[String, String],
+          body: Option[String]
+        ): UIO[Response] =
+          ZIO.effectTotal {
+            Response(
+              status = resStatus,
+              headers = resHeaders,
+              body = resBody
+            )
+          }
+    }
