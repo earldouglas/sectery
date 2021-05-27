640ef818d060d88513a689244539ccd0c81e102b
James Earl Douglas <james@earldouglas.com>
2021-05-27 05:28:06 -0700

    Extract TestHttp into its own file
    This better reflects the structure in *src/main* and removes some noise
    from *MessagesQueuesSpec.scala*.

---
+++ src/test/scala/sectery/TestHttp.scala
@@ -0,0 +1,34 @@
+package sectery
+
+import zio._
+
+object TestHttp:
+
+  def apply(): ULayer[Has[Http.Service]] =
+    apply(
+      resStatus = 200,
+      resHeaders = Map.empty,
+      resBody = ""
+    )
+
+  def apply(
+    resStatus: Int,
+    resHeaders: Map[String, String],
+    resBody: String
+  ): ULayer[Has[Http.Service]] =
+    ZLayer.succeed {
+      new Http.Service:
+        def request(
+          method: String,
+          url: String,
+          headers: Map[String, String],
+          body: Option[String]
+        ): UIO[Response] =
+          ZIO.effectTotal {
+            Response(
+              status = resStatus,
+              headers = resHeaders,
+              body = resBody
+            )
+          }
+    }
