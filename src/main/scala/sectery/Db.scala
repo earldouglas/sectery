4272fc4ca2fc4a13768878fc1f9cc35dbc8d8069
James Earl Douglas <james@earldouglas.com>
2021-06-15 13:39:30 -0700

    Configure continuous deployment to Heroku
    This makes a few changes to allow sectery to deploy to and run on
    Heroku:
    
    * Drop unused forked runtime environment variables, since these are
      inherited from the parent environment
    * Change root log level from `info` to `warn` to limit volume and exposure
    * Add support for Postgres, and use it for production
        * https://devcenter.heroku.com/articles/heroku-postgresql#connecting-in-java
    * Use sbt-native-packager to package as a Java app, with scripts
      generated in *target/universal/stage/bin/*
    * Configure as a `worker` Heroku process type
        * https://devcenter.heroku.com/articles/procfile#other-process-types
    * Deploy to Heroku when changes are pushed to the `master` branch

--- src/main/scala/sectery/Db.scala
+++ src/main/scala/sectery/Db.scala
@@ -1,5 +1,6 @@
 package sectery
 
+import java.net.URI
 import java.sql._
 import zio._
 
@@ -13,10 +14,15 @@ object Db:
   val live: ULayer[Has[Service]] =
     ZLayer.succeed {
       new Service:
-        Class.forName("org.sqlite.JDBC");
-        lazy val dbPath = sys.env("SECTERY_DB")
+        lazy val dbUri = new URI(sys.env("DATABASE_URL"))
+        lazy val username = dbUri.getUserInfo().split(":")(0)
+        lazy val password = dbUri.getUserInfo().split(":")(1)
+        lazy val dbUrl =
+          s"jdbc:postgresql://${dbUri.getHost()}:${dbUri.getPort()}${dbUri.getPath()}?sslmode=require"
+
         lazy val conn: Connection =
-          DriverManager.getConnection(s"jdbc:sqlite:${dbPath}")
+          DriverManager.getConnection(dbUrl, username, password)
+
         override def query[A](k: Connection => A): Task[A] =
           ZIO.effect(k(conn))
     }
