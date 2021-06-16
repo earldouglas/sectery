6c7f581e9548e78af0ce564f10d022faa03e02a6
James Earl Douglas <james@earldouglas.com>
2021-06-16 04:55:12 -0700

    Clean up the lazy production db connection code

--- src/main/scala/sectery/Db.scala
+++ src/main/scala/sectery/Db.scala
@@ -14,14 +14,20 @@ object Db:
   val live: ULayer[Has[Service]] =
     ZLayer.succeed {
       new Service:
-        lazy val dbUri = new URI(sys.env("DATABASE_URL"))
-        lazy val username = dbUri.getUserInfo().split(":")(0)
-        lazy val password = dbUri.getUserInfo().split(":")(1)
-        lazy val dbUrl =
-          s"jdbc:postgresql://${dbUri.getHost()}:${dbUri.getPort()}${dbUri.getPath()}?sslmode=require"
+        // this is lazy so that it doesn't get evaluated during testing
+        lazy val conn: Connection = {
 
-        lazy val conn: Connection =
+          // for info about the DATABASE_URL env var, see
+          // https://devcenter.heroku.com/articles/heroku-postgresql#connecting-in-java
+          val dbUri = new URI(sys.env("DATABASE_URL"))
+
+          val username = dbUri.getUserInfo().split(":")(0)
+          val password = dbUri.getUserInfo().split(":")(1)
+          val dbUrl =
+            s"jdbc:postgresql://${dbUri.getHost()}:${dbUri.getPort()}${dbUri.getPath()}?sslmode=require"
+
           DriverManager.getConnection(dbUrl, username, password)
+        }
 
         override def query[A](k: Connection => A): Task[A] =
           ZIO.effect(k(conn))
