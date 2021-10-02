5aef2124f667594331f429d46773aac6f88e4093
James Earl Douglas <james@earldouglas.com>
2021-10-02 16:33:57 -0700

    Add support for passing a time zone to @time (#256)

--- src/main/scala/sectery/producers/Time.scala
+++ src/main/scala/sectery/producers/Time.scala
@@ -15,18 +15,30 @@ import zio.ZIO
 
 object Time extends Producer:
 
+  private val time = """^@time(\s+([^\s]+)\s*)?$""".r
+
   override def help(): Iterable[Info] =
-    Some(Info("@time", "@time"))
+    Some(
+      Info(
+        "@time",
+        "@time [zone], e.g. @time, @time MST, @time GMT-7, @time America/Phoenix"
+      )
+    )
+
+  private def getTime(zone: String): URIO[Has[Clock], String] =
+    for
+      millis <- Clock.currentTime(TimeUnit.MILLISECONDS)
+      date = new Date(millis)
+      sdf = new SimpleDateFormat("EEE, d MMM yyyy, kk:mm zzz")
+      _ = sdf.setTimeZone(TimeZone.getTimeZone(zone))
+      pretty = sdf.format(date)
+    yield pretty
 
   override def apply(m: Rx): URIO[Has[Clock], Iterable[Tx]] =
     m match
       case Rx(c, _, "@time") =>
-        for
-          millis <- Clock.currentTime(TimeUnit.MILLISECONDS)
-          date = new Date(millis)
-          sdf = new SimpleDateFormat("EEE, d MMM yyyy, kk:mm zzz")
-          _ = sdf.setTimeZone(TimeZone.getTimeZone("America/Phoenix"))
-          pretty = sdf.format(date)
-        yield Some(Tx(c, pretty))
+        getTime("America/Phoenix").map(t => Some(Tx(c, t)))
+      case Rx(c, _, time(_, zone)) =>
+        getTime(zone).map(t => Some(Tx(c, t)))
       case _ =>
         ZIO.succeed(None)
