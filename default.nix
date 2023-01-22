c460cac29c8a1098c3ede9509dca1c119d2de1a2
James Earl Douglas <james@earldouglas.com>
2023-01-22 10:00:08 -0700

    Add a Nix derivation based on sbt-derivation
    See <https://github.com/zaninime/sbt-derivation> for details.

---
+++ default.nix
@@ -0,0 +1,26 @@
+let
+  sbt-derivation = import (builtins.fetchTarball {
+    url =
+      "https://github.com/zaninime/sbt-derivation/archive/1ef212261cf7ad878c253192a1c8171de4d75b6d.tar.gz";
+    sha256 = "1mz2s4hajc9cnrfs26d99ap4gswcidxcq441hg3aplnrmzrxbqbp";
+  });
+in { pkgs ? import <nixpkgs> { overlays = [ sbt-derivation ]; } }:
+pkgs.sbt.mkDerivation {
+
+  pname = "sectery";
+  version = "1.0.0";
+
+  depsSha256 = "sha256-Y4m/gql9SKhHH8kRmKte2R/gXFd6NfORVcofDkmyb8M=";
+
+  src = ./.;
+
+  buildPhase = ''
+    sbt assembly
+  '';
+
+  installPhase = ''
+    mkdir -p $out/
+    cp modules/irc/target/scala-*/irc.jar $out/
+    cp modules/producers/target/scala-*/producers.jar $out/
+  '';
+}
