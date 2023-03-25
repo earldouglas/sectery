f3ee82de795a6b35ad0fed1738975793dcf1afe9
James Earl Douglas <james@earldouglas.com>
2023-03-25 11:22:02 -0700

    Revert "Revert to environment nixpkgs"
    This reverts commit efe195fbdc5cc4170692df3352d03b910643f279.

--- default.nix
+++ default.nix
@@ -1,9 +1,21 @@
 let
 
-  sbt-derivation-src = builtins.fetchTarball {
-    url = "https://github.com/zaninime/sbt-derivation/archive/92d6d6d.tar.gz";
-    sha256 = "0hlpq1qzzvmswal3x02sv8hkl53bs9zrb62smwj3gnjm5a2qbi7s";
-  };
+  nixpkgs-src =
+    builtins.fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/a7ecde8.tar.gz";
+      sha256 = "162dywda2dvfj1248afxc45kcrg83appjd0nmdb541hl7rnncf02";
+    };
+
+  nixpkgs =
+    (import nixpkgs-src) {
+      overlays = [ sbt-derivation sbt-overlay ];
+    };
+
+  sbt-derivation-src =
+    builtins.fetchTarball {
+      url = "https://github.com/zaninime/sbt-derivation/archive/92d6d6d.tar.gz";
+      sha256 = "0hlpq1qzzvmswal3x02sv8hkl53bs9zrb62smwj3gnjm5a2qbi7s";
+    };
 
   sbt-derivation = import "${sbt-derivation-src}/overlay.nix";
 
@@ -14,27 +26,24 @@ let
       };
     };
 
-in {
-  pkgs ? import <nixpkgs> {
-    overlays = [ sbt-derivation sbt-overlay ];
+in
+
+  nixpkgs.mkSbtDerivation {
+
+    pname = "sectery";
+    version = "1.0.0";
+
+    depsSha256 = "sha256-YqB8oXCL3eKZBxCPpKKM/A0AjWQahtzHpZ6CzCUtmGM";
+
+    src = ./.;
+
+    buildPhase = ''
+      sbt assembly
+    '';
+
+    installPhase = ''
+      mkdir -p $out/
+      cp modules/irc/target/scala-*/irc.jar $out/
+      cp modules/producers/target/scala-*/producers.jar $out/
+    '';
   }
-}:
-pkgs.mkSbtDerivation {
-
-  pname = "sectery";
-  version = "1.0.0";
-
-  depsSha256 = "sha256-F+ZeLifeQlXJg7ympDWeSN2Bv7ZkMD+rlfZoDBhHrCg=";
-
-  src = ./.;
-
-  buildPhase = ''
-    sbt assembly
-  '';
-
-  installPhase = ''
-    mkdir -p $out/
-    cp modules/irc/target/scala-*/irc.jar $out/
-    cp modules/producers/target/scala-*/producers.jar $out/
-  '';
-}
