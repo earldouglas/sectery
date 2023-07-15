9c6f1eeec4da836c058f5b1cf985520d8744ec3f
James Earl Douglas <james@earldouglas.com>
2023-07-15 08:07:12 -0700

    sbt-tpolecat: 0.4.3 -> 0.4.4

--- default.nix
+++ default.nix
@@ -35,7 +35,7 @@ in
     pname = "sectery";
     version = "1.0.0";
 
-    depsSha256 = "sha256-3qfaxdAawfkcnFvttm3PzJ0J0A/ijS2KvTu7tsKEV9k=";
+    depsSha256 = "sha256-AuYrsk1b4TxdCID4TWXwwVm3tQrQkxFHJllCp2NUrCQ=";
 
     src = ./.;
 
