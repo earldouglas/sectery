44f795a65c4d9e81065036045d0964d45036797b
James Earl Douglas <james@earldouglas.com>
2023-09-30 06:39:57 -0700

    amqp-client: 5.18.0 -> 5.19.0

--- default.nix
+++ default.nix
@@ -35,7 +35,7 @@ in
     pname = "sectery";
     version = "1.0.0";
 
-    depsSha256 = "sha256-sfxSZOg2AsdMMQftZjLT7RvjXq3ULU1XoOdK8lry6L4=";
+    depsSha256 = "sha256-nwU+EgNKKcpfoKtXVqwW+eYnQ0J+BlZI3GznmkSw1Tk=";
 
     src = ./.;
 
