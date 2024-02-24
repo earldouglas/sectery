68257744fcb599183a4dde4dfec222d551542c54
James Earl Douglas <james@earldouglas.com>
2024-02-24 09:10:32 -0700

    zio-logging: 2.2.1 -> 2.2.2 (#560)

--- default.nix
+++ default.nix
@@ -35,7 +35,7 @@ in
     pname = "sectery";
     version = "1.0.0";
 
-    depsSha256 = "sha256-UXQWgllZzb3LvHs7ZD4gyfPNM1DoZBytuhSqMe1QErA=";
+    depsSha256 = "sha256-PqMaAeHls0ra61I+JGL2DKig6jmGEAcwOfJB/qitUaQ=";
 
     src = ./.;
 
