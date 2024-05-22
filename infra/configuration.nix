286f6455c52973eb47617c119611d196ca1cb9f4
James Earl Douglas <james@earldouglas.com>
2024-05-22 11:19:22 -0700

    Deploy latest revision

--- infra/configuration.nix
+++ infra/configuration.nix
@@ -19,8 +19,8 @@ in {
         import (pkgs.fetchFromGitHub {
           owner = "earldouglas";
           repo = "sectery";
-          rev = "8aec144b886a908ed2555d341e112fa0d35cf84d";
-          sha256 = "0x7pgqqap27qqyhs4194rrfc3gbng9936i3brajraidki6hfw41l";
+          rev = "e789a45617af912ff62b12d0d96b99dc548972bc";
+          sha256 = "0qgljmk5mzp0fn6c3skl21kn89ip4gpzkn6hby95w202mzrjrpa8";
         });
 
       slf4jSimple =
