29998ef3a15dcef011b7e014dcbbee190f68d0b3
James Earl Douglas <james@earldouglas.com>
2024-09-15 16:16:02 -0700

    munit: 1.0.1 -> 1.0.2 (#628)

--- default.nix
+++ default.nix
@@ -37,7 +37,7 @@ in
     pname = "sectery";
     version = "1.0.0";
 
-    depsSha256 = "sha256-PMp1Ty/Dp3stC6E7NKvFVcdqVEPHtPtKc5q/IqCiyd0=";
+    depsSha256 = "sha256-EuKPEWrSJoP4ZeXsjewL8/hkhOLxu5Z5oxeESIduEn0=";
 
     src = ./.;
 
