2449bcc53b58cdd89653bcad640cf469ac7afcb2
James Earl Douglas <james@earldouglas.com>
2024-11-23 17:14:59 -0700

    zio: 2.1.12 -> 2.1.13 (#656)

--- default.nix
+++ default.nix
@@ -37,7 +37,7 @@ in
     pname = "sectery";
     version = "1.0.0";
 
-    depsSha256 = "sha256-mbaOfNV3Ij09vdYzQi3FnfOhGLOKuS3bz32rsmsh4RM=";
+    depsSha256 = "sha256-X7BkIH3g27ejA3FeuBFlCVT3Mu26Uc3Ayr4gMW4Rars=";
 
     src = ./.;
 
