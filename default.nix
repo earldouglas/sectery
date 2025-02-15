cb987b5e0a5014f3aff0f36c04a078280c3fd129
James Earl Douglas <james@earldouglas.com>
2025-02-15 06:51:53 -0700

    zio-json: 0.7.16 -> 0.7.21

--- default.nix
+++ default.nix
@@ -38,7 +38,7 @@ in
     pname = "sectery";
     version = "1.0.0";
 
-    depsSha256 = "sha256-0lQmwTJhJZYs3xP19Kpntr6CYE+Cv39YzK6pEfzI8FM=";
+    depsSha256 = "sha256-cN69lKUYr0xFYn8K1EyzyuLTreXghcGnY0j9xr8GLUA=";
 
     src = ./.;
 
