b5eb7cdd6ba5fcfad8a18a56d6993cef208adca4
James Earl Douglas <james@earldouglas.com>
2025-05-18 13:33:56 -0700

    Update deps sha

--- default.nix
+++ default.nix
@@ -38,7 +38,7 @@ in
     pname = "sectery";
     version = "1.0.0";
 
-    depsSha256 = "sha256-7eoXrpBG19YbOrUKtHyMfuGCzzuKnPuSMv1R8Dy5d5w=";
+    depsSha256 = "sha256-GXa3Hxx+/fWx5c14eoI0qL0nWNs5RxJhF3PcWKUnoPY=";
 
     src = ./.;
 
