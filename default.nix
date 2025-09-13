eed49f5711420d38f1066780110d449c7c764096
James Earl Douglas <james@earldouglas.com>
2025-09-13 10:05:13 -0700

    Update deps sha

--- default.nix
+++ default.nix
@@ -39,7 +39,7 @@ pkgs.mkSbtDerivation {
   pname = "sectery";
   version = "0.1.0-SNAPSHOT";
 
-  depsSha256 = "sha256-vi4fi3MzIWquDxFvPgXqNkHiVSCZFPIeJMdRUH6HI8s=";
+  depsSha256 = "sha256-ztvifV+qt15qS/JbPi8QFuC1328ettynb7URlrzeJUE=";
 
   src = ./.;
 
