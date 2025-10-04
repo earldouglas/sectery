9fa1f697c2b1710aea4f6e32ad51e9ae33317eb9
James Earl Douglas <james@earldouglas.com>
2025-10-04 15:48:05 -0700

    sbt-native-packager: 1.11.3 -> 1.11.4 (#758)

--- default.nix
+++ default.nix
@@ -39,7 +39,7 @@ pkgs.mkSbtDerivation {
   pname = "sectery";
   version = "0.1.0-SNAPSHOT";
 
-  depsSha256 = "sha256-nB2K2L9LvEZhnkYpJ+w8NYM7qx7nGA4mHsSRRhLpRU8=";
+  depsSha256 = "sha256-P4vrtvHE8B06f9jpnStPQWx4ucowQ58QEvB34QtiRtg=";
 
   src = ./.;
 
