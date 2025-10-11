755f930f888f1f451d991b65895988a207b9f525
James Earl Douglas <james@earldouglas.com>
2025-10-11 06:28:50 -0700

    sbt-scalafix: 0.14.3 -> 0.14.4

--- default.nix
+++ default.nix
@@ -39,7 +39,7 @@ pkgs.mkSbtDerivation {
   pname = "sectery";
   version = "0.1.0-SNAPSHOT";
 
-  depsSha256 = "sha256-qqHyJakUnKtgbxLcLa8o/MAa2ddcRJ72TqOdkxNUfpE=";
+  depsSha256 = "sha256-0DEGAF2bElc0xZlLkUfgHy+/IjBqZJia9Yr3u7zIASg=";
 
   src = ./.;
 
