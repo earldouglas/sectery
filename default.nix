b48a04d90eb7a6df2e67e901f95cc860a1d0fc19
James Earl Douglas <james@earldouglas.com>
2025-10-18 10:04:33 -0700

    zio: 2.1.21 -> 2.1.22 (#763)

--- default.nix
+++ default.nix
@@ -39,7 +39,7 @@ pkgs.mkSbtDerivation {
   pname = "sectery";
   version = "0.1.0-SNAPSHOT";
 
-  depsSha256 = "sha256-cvrvzRYd8Pp6wXjnqHf/tMe2ot2YVnYTv/KU3hJRg0o=";
+  depsSha256 = "sha256-NumMaGWbuLMmQhc/s2mUEo0U5yqgijQyK0dw2x4zi44=";
 
   src = ./.;
 
