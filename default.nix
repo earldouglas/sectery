c9572a56bc95b6d964ebbad8cf6f159403918730
James Earl Douglas <james@earldouglas.com>
2025-10-18 07:04:20 -0700

    amqp-client: 5.26.0 -> 5.27.0

--- default.nix
+++ default.nix
@@ -39,7 +39,7 @@ pkgs.mkSbtDerivation {
   pname = "sectery";
   version = "0.1.0-SNAPSHOT";
 
-  depsSha256 = "sha256-o7yYS61jK4+KkjkRn9OJwvLEdrnxO6RP9hklvkoVnkI=";
+  depsSha256 = "sha256-cvrvzRYd8Pp6wXjnqHf/tMe2ot2YVnYTv/KU3hJRg0o=";
 
   src = ./.;
 
