9dd4e2686d251664e5f216f72e275058475912c5
James Earl Douglas <james@earldouglas.com>
2025-11-02 07:54:28 -0700

    zio-json: 0.7.44 -> 0.7.45 (#770)

--- default.nix
+++ default.nix
@@ -39,7 +39,7 @@ pkgs.mkSbtDerivation {
   pname = "sectery";
   version = "0.1.0-SNAPSHOT";
 
-  depsSha256 = "sha256-cqFGbSUbkRi4FnJuHKFDfSadASHJ4kv6hR3WIi+P0MU=";
+  depsSha256 = "sha256-eeoqD+uxnOAY8IabIa1OzmFUlyApfJuuzhp21sykatU=";
 
   src = ./.;
 
