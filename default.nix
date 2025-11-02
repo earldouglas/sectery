14b6a352b46fb76d072cf891ad10cde0a2abe264
James Earl Douglas <james@earldouglas.com>
2025-11-02 06:55:23 -0700

    tyrus-standalone-client: 2.2.0 -> 2.2.1

--- default.nix
+++ default.nix
@@ -39,7 +39,7 @@ pkgs.mkSbtDerivation {
   pname = "sectery";
   version = "0.1.0-SNAPSHOT";
 
-  depsSha256 = "sha256-eeoqD+uxnOAY8IabIa1OzmFUlyApfJuuzhp21sykatU=";
+  depsSha256 = "sha256-FGLQfx7pL3bYKb90i80lyTo+5cCPFsBOW9H4CBzecyc=";
 
   src = ./.;
 
