0e9beffda707acdfb0520f99872147e8d2c5acfb
James Earl Douglas <james@earldouglas.com>
2025-11-15 07:57:18 -0700

    logback-classic: 1.5.20 -> 1.5.21

--- default.nix
+++ default.nix
@@ -39,7 +39,7 @@ pkgs.mkSbtDerivation {
   pname = "sectery";
   version = "0.1.0-SNAPSHOT";
 
-  depsSha256 = "sha256-4zJCsnFXLoYfaDnXTXEMgLYOV22yNtU+kgdT9bYyelg=";
+  depsSha256 = "sha256-bv7oycDAMpaQMw4IfETtONDa4qV6IDpojYMmx1VO1OQ=";
 
   src = ./.;
 
