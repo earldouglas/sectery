599e298efaa357e3af408d9c0da2f242e9e77942
James Earl Douglas <james@earldouglas.com>
2026-01-03 08:14:04 -0700

    jsoup: 1.21.2 -> 1.22.1

--- default.nix
+++ default.nix
@@ -39,7 +39,7 @@ pkgs.mkSbtDerivation {
   pname = "sectery";
   version = "0.1.0-SNAPSHOT";
 
-  depsSha256 = "sha256-BWQj+JZcvV/rC9OYafBha/CdMRpOkE0tD5DFEzhnZTE=";
+  depsSha256 = "sha256-YH1GzgYZNBkFw1OdJ1jj2cd8CzgzM1ebRuFsH14r+Do=";
 
   src = ./.;
 
