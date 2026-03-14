dadc8ffe9cdf9a3f5a97d67df569b85e320bea0e
James Earl Douglas <james@earldouglas.com>
2026-03-14 08:59:15 -0700

    sbt: 1.12.3 -> 1.12.5

--- default.nix
+++ default.nix
@@ -39,7 +39,7 @@ pkgs.mkSbtDerivation {
   pname = "sectery";
   version = "0.1.0-SNAPSHOT";
 
-  depsSha256 = "sha256-BU9TALxMp/oVIR60oP7crp7ZOIB5M0I7u67iGjbky4U=";
+  depsSha256 = "sha256-e9wIB83g2LlmDLMDAQC3s/0P1J1i1XdPgZqKw891TFY=";
 
   src = ./.;
 
