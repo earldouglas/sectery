39096d13a4af1ace3e3aa045ef31d7cfa052744b
James Earl Douglas <james@earldouglas.com>
2026-03-21 08:29:20 -0700

    munit: 1.2.2 -> 1.2.4

--- default.nix
+++ default.nix
@@ -39,7 +39,7 @@ pkgs.mkSbtDerivation {
   pname = "sectery";
   version = "0.1.0-SNAPSHOT";
 
-  depsSha256 = "sha256-iNl/p1254A+iyDoR80S+IGceIqzZexvhIeMBH0ytA7U=";
+  depsSha256 = "sha256-fF4zm8XRrBRBhJ+Ep3XWT4VZHlGuZ/OynuHiCB/2aWI=";
 
   src = ./.;
 
