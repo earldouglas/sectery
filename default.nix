7e301fa4d646bf57dd0c0195852ae0839cd77225
James Earl Douglas <james@earldouglas.com>
2026-03-21 08:29:08 -0700

    scala3-library: 3.8.1 -> 3.8.2

--- default.nix
+++ default.nix
@@ -39,7 +39,7 @@ pkgs.mkSbtDerivation {
   pname = "sectery";
   version = "0.1.0-SNAPSHOT";
 
-  depsSha256 = "sha256-s+iPrp9KnGV7Ed0eExVxrxEZVnjCw5leAMmm13kYc3I=";
+  depsSha256 = "sha256-iNl/p1254A+iyDoR80S+IGceIqzZexvhIeMBH0ytA7U=";
 
   src = ./.;
 
